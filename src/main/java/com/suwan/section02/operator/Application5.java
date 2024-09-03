7e35705f91107f36af72b005cd74b686634fede7
박수완 <kungk7952@gmail.com>
2024-09-03 17:18:49 +0900

    연습문제 수정

--- src/main/java/com/suwan/section02/operator/Application5.java
+++ src/main/java/com/suwan/section02/operator/Application5.java
@@ -10,10 +10,10 @@ public class Application5 {
         double c1 = (double) c;
         double d1 = (double) d;
 
-        System.out.println(a1);
 
-        double max = (a1 > b1) ? a1 : (b1 > c1) ? b1 : (c1 > d1) ? c1 : d1;
-        double min = (a1 < b1) ? a1 : (b1 < c1) ? b1 : (c1 < d1) ? c1 : d1;
+        double max = ((a1 > b1) && (a1 > c1) && (a1 > d1)) ? a1 : ((b1 > a1) && (b1 > c1) && (b1 > d1)) ? b1 : ((c1 > a1) && (c1 > b1) && (c1 > d1)) ? c1 : ((d1 > a1) && (d1 > b1) && (d1 > c1)) ? d1 : a1;
+        double min = ((a1 < b1) && (a1 < c1) && (a1 < d1)) ? a1 : ((b1 < a1) && (b1 < c1) && (b1 < d1)) ? b1 : ((c1 < a1) && (c1 < b1) && (c1 < d1)) ? c1 : ((d1 < a1) && (d1 < b1) && (d1 < c1)) ? d1 : a1;
+
 
         System.out.println("최대값은 " + max + " 입니다." + " 최소값은 " + min + "입니다.");
         System.out.println("최대값과 최소값의 차이는 " + (max - min) + " 입니다.");
